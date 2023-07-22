e3706b5d7e18907188a632b1eee845712f46ca11
HertonJP <125948356+HertonJP@users.noreply.github.com>
2023-07-22 15:32:51 +0700

    Enemy AI Shooting

---
+++ Assets/Scripts/EnemyProjectiles.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectiles : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+
+    private Transform target;
+
+    [SerializeField] private float projectilesSpeed = 5f;
+    [SerializeField] private int projectilesDamage = 1;
+
+    
+    
+    public void SetTarget(Transform target)
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            target = player.transform;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (!target)
+        {
+            return;
+        }
+        Vector2 direction = (target.position - transform.position).normalized;
+
+        rb.velocity = direction * projectilesSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+       // other.gameObject.GetComponent<enemyHealth>().TakeDamage(projectilesDamage);
+        Destroy(gameObject);
+    }
+}
